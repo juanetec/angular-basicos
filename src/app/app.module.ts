c6fa01a210ba1135aec3dbeced21dbcb6a94c799
jramirez <juan@etecsoft.com>
2022-08-21 11:27:29 +0100

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { HeroesModules } from './heroes/heroes.modulo';
+import { ContadorModules } from './contador/contador.modulo';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModules,
+    ContadorModules
   ],
   providers: [],
   bootstrap: [AppComponent]
